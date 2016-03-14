29191937b05b927843890c800db602b51fd91867
Jonas Rafn <Jonas Rafn>
2016-03-14 19:02:57 +0100

    Added CollectiveMemory

---
+++ src/ai/Main.java
@@ -0,0 +1,95 @@
+package ai;
+
+import aiantwars.EAction;
+import aiantwars.EAntType;
+import aiantwars.IAntAI;
+import aiantwars.IAntInfo;
+import aiantwars.IEgg;
+import aiantwars.ILocationInfo;
+import java.util.List;
+import java.util.Map;
+import java.util.Random;
+import memory.CollectiveMemory;
+import memory.Position;
+import memory.Tile;
+
+public class Main implements IAntAI {
+
+    private final Random rnd = new Random();
+    private final CollectiveMemory collectiveMemory = CollectiveMemory.getInstance();
+
+    @Override
+    public EAction chooseAction(IAntInfo thisAnt, ILocationInfo thisLocation, List<ILocationInfo> visibleLocations, List<EAction> possibleActions) {
+        collectiveMemory.addVisibleLocations(visibleLocations);
+
+        for (Map.Entry pair : collectiveMemory.getMemory().entrySet()) {
+            Position pos = (Position) pair.getKey();
+            Tile tile = (Tile) pair.getValue();
+
+            System.out.println("Map position (" + pos.getxPos() + ", " + pos.getyPos() + ")" + ", FoodCount: " + tile.getFoodCount() + ", Ant: Not Supported"
+                    + ", IsFilled: " + tile.isFilled() + ", IsRock: " + tile.isRock());
+        }
+
+        /**
+         * Possible actions
+         *
+         * TurnLeft("Turn Left <-")
+         * TurnRight("Turn Right ->") MoveForward("Move forward")
+         * MoveBackward("Move backward") Attack("Attack") PickUpFood("Pick up
+         * food") DropFood("Drop food") EatFood("Eat food") DigOut("Dig out")
+         * DropSoil("Drop soil") LayEgg("Lay egg") Pass("Pass")
+         *
+         */
+        EAction action = null;
+        if (possibleActions.contains(EAction.EatFood) && thisAnt.getHitPoints() < 10) {
+            action = EAction.EatFood;
+        } else if (possibleActions.contains(EAction.LayEgg)) {
+            action = EAction.LayEgg;
+        } else if (possibleActions.contains(EAction.Attack) && visibleLocations.get(0).getAnt().getTeamInfo().getTeamID() != thisAnt.getTeamInfo().getTeamID()) {
+            action = EAction.Attack;
+        } else if (possibleActions.contains(EAction.PickUpFood) && thisAnt.getFoodLoad() < 15) {
+            action = EAction.PickUpFood;
+        } else {
+            action = possibleActions.get(rnd.nextInt(possibleActions.size()));
+            if (action == EAction.Attack) {
+                action = EAction.Pass;
+            }
+        }
+        StringBuilder actions = new StringBuilder();
+        for (EAction a : possibleActions) {
+            actions.append(a.toString());
+            actions.append(", ");
+        }
+        System.out.println(actions.toString());
+        System.out.println("ID: " + thisAnt.antID() + " chooseAction: " + action);
+        return action;
+    }
+
+    @Override
+    public void onStartTurn(IAntInfo thisAnt, int turn) {
+        System.out.println("ID: " + thisAnt.antID() + " onStartTurn(" + turn + ")");
+    }
+
+    @Override
+    public void onAttacked(IAntInfo thisAnt, int dir, IAntInfo attacker, int damage) {
+        System.out.println("ID: " + thisAnt.antID() + " onAttacked: " + damage + " damage");
+    }
+
+    @Override
+    public void onDeath(IAntInfo thisAnt) {
+        System.out.println("ID: " + thisAnt.antID() + " onDeath");
+    }
+
+    @Override
+    public void onLayEgg(IAntInfo thisAnt, List<EAntType> types, IEgg egg) {
+        EAntType type = types.get(rnd.nextInt(types.size()));
+        System.out.println("ID: " + thisAnt.antID() + " onLayEgg: " + type);
+        egg.set(type, this);
+    }
+
+    @Override
+    public void onHatch(IAntInfo thisAnt, ILocationInfo thisLocation, int worldSizeX, int worldSizeY) {
+        System.out.println("ID: " + thisAnt.antID() + " onHatch");
+    }
+
+}
---
+++ src/memory/Tile.java
@@ -0,0 +1,55 @@
+package memory;
+
+import aiantwars.IAntInfo;
+
+/**
+ *
+ * @author Jonas Rafn
+ */
+public class Tile {
+
+    private int foodCount;
+    private IAntInfo ant;
+    private boolean isFilled;
+    private boolean isRock;
+
+    public Tile(int foodCount, IAntInfo ant, boolean isFilled, boolean isRock) {
+        this.foodCount = foodCount;
+        this.ant = ant;
+        this.isFilled = isFilled;
+        this.isRock = isRock;
+    }
+
+    public int getFoodCount() {
+        return foodCount;
+    }
+
+    public void setFoodCount(int foodCount) {
+        this.foodCount = foodCount;
+    }
+
+    public IAntInfo getAnt() {
+        return ant;
+    }
+
+    public void setAnt(IAntInfo ant) {
+        this.ant = ant;
+    }
+
+    public boolean isFilled() {
+        return isFilled;
+    }
+
+    public void setIsFilled(boolean isFilled) {
+        this.isFilled = isFilled;
+    }
+
+    public boolean isRock() {
+        return isRock;
+    }
+
+    public void setIsRock(boolean isRock) {
+        this.isRock = isRock;
+    }
+
+}
