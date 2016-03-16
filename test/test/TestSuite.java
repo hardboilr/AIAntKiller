ee3e5db942b4ae956a78a9437d05d547e1d206e2
Jonas Rafn <Jonas Rafn>
2016-03-16 12:14:20 +0100

    Added CollectiveMemoryTest to TestSuite

--- test/test/TestSuite.java
+++ test/test/TestSuite.java
@@ -4,7 +4,7 @@ import org.junit.runner.RunWith;
 import org.junit.runners.Suite;
 
 @RunWith(Suite.class)
-@Suite.SuiteClasses({test.CollectFoodTest.class, test.ComparatorTest.class, test.CalcTest.class})
+@Suite.SuiteClasses({test.CollectFoodTest.class, test.ComparatorTest.class, test.CalcTest.class, test.CollectiveMemorytest.class})
 public class TestSuite {
-    
+
 }
